8e53af1fad20b9a6f1ac49c7a6c9c3cdee56df16
Doo Lee <dlengjs12@naver.com>
2020-10-12 02:27:33 +0900

    두스타그램 헤더 레이아웃 작성 및 profile로직 구현 파싱투해커랭크 로직 수정

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import {
+    LOG_IN,
+    CREATE_ACCOUNT,
+    CONFIRM_SECRET,
+    LOCAL_LOG_IN
+} from "./AuthQueries";
 import { toast } from "react-toastify";
 
 export default () => {
@@ -10,9 +15,9 @@ export default () => {
     const username = useInput("");
     const firstName = useInput("");
     const lastName = useInput("");
-    const secret = useInput(""); 
-    const email = useInput("itnico.las.me@gmail.com");
-    const requestSecretMutation = useMutation(LOG_IN, {        
+    const secret = useInput("");
+    const email = useInput("");
+    const requestSecretMutation = useMutation(LOG_IN, {
         variables: { email: email.value }
     });
 
@@ -24,7 +29,15 @@ export default () => {
             lastName: lastName.value
         }
     });
-    const onSubmit = async e => { 
+
+    const confirmSecretMutation = useMutation(CONFIRM_SECRET, {
+        variables: {
+            email: email.value,
+            secret: secret.value
+        }
+    });
+    const localLogInMutation = useMutation(LOCAL_LOG_IN);
+    const onSubmit = async e => {
         e.preventDefault();
         if (action === "logIn") {
             // requestSecret[0]();
@@ -37,7 +50,7 @@ export default () => {
                     if (!requestSecret) {
                         toast.error("You dont have an account yet, create one");
                         setTimeout(() => setAction("signUp"), 3000);
-                    }else {
+                    } else {
                         toast.success("Check your inbox for your login secret");
                         setAction("confirm");
                     }
@@ -60,17 +73,35 @@ export default () => {
                         data: { createAccount }
                     } = await createAccountMutation[0]();
                     if (!createAccount) {
-                      toast.error("Can't create account");
+                        toast.error("Can't create account");
                     } else {
-                      toast.success("Account created! Log In now");
-                      setTimeout(() => setAction("logIn"), 3000);
+                        toast.success("Account created! Log In now");
+                        setTimeout(() => setAction("logIn"), 3000);
                     }
-                  } catch (e) {
+                } catch (e) {
                     toast.error(e.message);
-                  }
+                }
             } else {
                 toast.error("All field are required");
             }
+        } else if (action === "confirm") {
+            if (secret.value !== "") {
+                try {
+                    const {
+                        data: { confirmSecret: token }
+                    } = await confirmSecretMutation[0]();
+                    console.log(token)
+                    if (token !== "" && token !== undefined) {
+                        localLogInMutation[0]({ variables: { token } });
+                    } else {
+                        throw Error();
+                    }
+                } catch(e) {
+                    console.log(e);
+                    toast.error("Cant confirm secret,check again");
+                }
+            }
+
         }
     };
 
@@ -82,8 +113,8 @@ export default () => {
             firstName={firstName}
             lastName={lastName}
             email={email}
-            secret={secret}             
-            onSubmit={onSubmit} 
+            secret={secret}
+            onSubmit={onSubmit}
         />
     );
 };
